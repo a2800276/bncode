1329e16665ff236bb62175dbd3992a9c9463b2f1
Ivo Georgiev <ivo@linvo.org>
2013-08-13 21:26:05 +0300

    Added a function current_result which allows reading partial buffers

--- bencode.js
+++ bencode.js
@@ -325,6 +325,10 @@ var Bdecode = function () {
     }
     return ctx.result()
   }
+  
+  this.current_result    = function () {
+    return ctx.result()
+  }
 
   this.decode = function(buf, encoding) {
     smachine.parse(buf, encoding)
